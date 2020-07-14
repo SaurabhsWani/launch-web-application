fcd4e33d8ad0cf58e1599564e51d7a0d35b17ece
Saurabh wani <66895714+SaurabhsWani@users.noreply.github.com>
2020-07-14 14:57:53 +0530

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My First Task</title>
+</head>
+<body style="background-color: lightgray;">
+	<center>
+		<h1 style="text-align: left">This is My Task 2nd</h1>
+		<img src="https://raw.githubusercontent.com/SaurabhsWani/myweb/master/img1.jpg" width="250px;" height="250px;">	
+	</center>
+</body>
+</html>
